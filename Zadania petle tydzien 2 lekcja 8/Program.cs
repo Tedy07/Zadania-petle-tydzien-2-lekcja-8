7dc1e1b612247f17c00d83c8f3b397987b57cd1d
Ted K <ted.k07@gmail.com>
2023-02-12 00:18:53 +0100

    First task floop for

--- Zadania petle tydzien 2 lekcja 8/Program.cs
+++ Zadania petle tydzien 2 lekcja 8/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ile jest liczb pierwszych w zakresie od 0 do 100.");
+
+            for (int i = 0; i < 100; i++)
+            {
+
+            }
+
+
+
+
+
         }
     }
 }
