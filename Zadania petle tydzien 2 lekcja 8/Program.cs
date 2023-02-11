9f5a0bb642d8b1fd1177c6b500b1f2c704b3ad5e
Ted K <ted.k07@gmail.com>
2023-02-11 23:44:55 +0100

    Add project files.

---
+++ Zadania petle tydzien 2 lekcja 8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zadania_petle_tydzien_2_lekcja_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
