0bc31a0f7d0806646ea7d056320b6618ab1dc324
Ted K <ted.k07@gmail.com>
2023-02-12 22:05:01 +0100

    Introducion and first variable

--- Zadania petle tydzien 2 lekcja 8/Program.cs
+++ Zadania petle tydzien 2 lekcja 8/Program.cs
@@ -5,9 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Ile jest liczb pierwszych w zakresie od 0 do 100.");
+            Console.WriteLine("Podaj zakres liczb do sprawdzenia");
+            
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            int no1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę: ");
+            int no2 = int.Parse(Console.ReadLine());
+
 
             for (int i = 0; i < 100; i++)
             {
+
+
 
             }
 
