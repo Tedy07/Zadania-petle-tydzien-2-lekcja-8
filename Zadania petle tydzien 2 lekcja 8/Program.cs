fd7dc604cbe071cd69a2e66293d00d95a9531376
Ted K <ted.k07@gmail.com>
2023-02-18 00:16:30 +0100

    Continue floop primary number

--- Zadania petle tydzien 2 lekcja 8/Program.cs
+++ Zadania petle tydzien 2 lekcja 8/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-          
+
 
             Console.WriteLine("Liczby pierwsze w zakresie od 0 do 100.");
             Console.WriteLine("Podaj zakres liczb do sprawdzenia");
-            
+
             Console.WriteLine("Podaj pierwszą liczbę: ");
             int i1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Podaj drugą liczbę: ");
@@ -17,33 +17,41 @@
             int counters = 0;
             //licznik
 
-            for ( i1 = i1 ; i1 <= i2; i1++)
+            for (int i = i1; i <= i2; i++)
             {
-                
+                int liczba;
                 counters++;
+
+
+                if (CzyPierwsza(liczba))
+                {
+                    Console.WriteLine("TAK");
+
+                }
+                else
+                {
+                    Console.WriteLine("NIE");
+                }
+
 
                 Console.WriteLine($"{i1} and {counters}");
 
-
-
-
-                //if (i1 % i2 == 0)
+                //if (i1 % i1 == 0 &&  )
                 //{
-
+                //    Console.WriteLine($"Reszta z dzielenia cyfry {i1} == 0 ");
                 //} else
                 //{
-
+                //    Console.WriteLine("W przeciwnym razie");
                 //}
-
-
-
-
             }
 
-
-
-
-
         }
+
+        static bool CzyPierwsza(int n)
+        {
+
+            return true;
+        }
+
     }
 }
