16dce02f50229c707f1d676c867cb4d68331d99d
Ted K <ted.k07@gmail.com>
2023-02-19 23:02:37 +0100

    This is version finisch

--- Zadania petle tydzien 2 lekcja 8/Program.cs
+++ Zadania petle tydzien 2 lekcja 8/Program.cs
@@ -4,9 +4,7 @@
     {
         static void Main(string[] args)
         {
-
-
-            Console.WriteLine("Liczby pierwsze w zakresie od 0 do 100.");
+            Console.WriteLine("Zadanie liczby pierwsze w zakresie od 0 do 100.");
             Console.WriteLine("Podaj zakres liczb do sprawdzenia");
 
             Console.WriteLine("Podaj pierwszą liczbę: ");
@@ -15,12 +13,11 @@
             int i2 = int.Parse(Console.ReadLine());
 
             int counters = 0;
-            //licznik
 
             for (int i = i1; i <= i2; i++)
             {
                 int liczba = i;
-               
+
                 if (CzyPierwsza(liczba))
                 {
                     Console.WriteLine($"TAK {liczba} jest liczbą pierwszą");
@@ -31,41 +28,28 @@
                 {
                     Console.WriteLine($"{liczba} NIE jest liczbą pierwszą");
                 }
-
-            
-
-                //if (i1 % i1 == 0 &&  )
-                //{
-                //    Console.WriteLine($"Reszta z dzielenia cyfry {i1} == 0 ");
-                //} else
-                //{
-                //    Console.WriteLine("W przeciwnym razie");
-                //}
             }
-
-
             Console.WriteLine($"W podanym przedziale od {i1} do {i2} liczb pierwszych jest: {counters} ");
         }
-
-        static bool CzyPierwsza(int n)  
+        static bool CzyPierwsza(int n)
         {
-            if (n == 1)
+            if (n <= 0)
+            {
+                return false;
+            }
+            else if (n == 1)
             {
                 return false;
             }
 
-
             for (int i = 2; i < n; i++)
             {
-                if (n%i == 0) 
-                {  
+                if (n % i == 0)
+                {
                     return false;
                 }
-
             }
-             
             return true;
         }
-
     }
 }
